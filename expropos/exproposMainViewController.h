235e2c8e3470d4755230fb76a16cd99492d4040e
guanbo <guanbo2002@gmail.com>
2012-05-29 17:43:15 +0800

    add main view

---
+++ expropos/exproposMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  exproposMainViewController.h
+//  expropos
+//
+//  Created by gbo on 12-5-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface exproposMainViewController : UIViewController
+
+@end
---
+++ expropos/exproposMainViewController.m
@@ -0,0 +1,43 @@
+//
+//  exproposMainViewController.m
+//  expropos
+//
+//  Created by gbo on 12-5-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "exproposMainViewController.h"
+
+@interface exproposMainViewController ()
+
+@end
+
+@implementation exproposMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
